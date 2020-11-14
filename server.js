493d258213215bd8f0c11c04c4dca0364dedfae4
frostyausty <priceaustin62@gmail.com>
2020-11-14 10:01:09 -0600

    rearranged files

---
+++ server.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
