4e4b71f66bbb2060828adb9c49667eba943ce058
frostyausty <priceaustin62@gmail.com>
2020-11-15 17:56:28 -0600

    added save note functionality

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,13 +1,21 @@
 //const path = require('path');
 const router = require('express').Router();
 const notes  = require('../../db/db.json');
-
+const createNewNote = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
-    console.log(notes);
     res.json(notes);
 });
 
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes)
+    
+    console.log('A new note was created');
+
+    res.json(note);
+});
 
 
 
