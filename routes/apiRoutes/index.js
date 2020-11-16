2dd98cb83cb9143baa9df53eddac1a53dc5ce3a1
frostyausty <priceaustin62@gmail.com>
2020-11-15 20:19:08 -0600

    created unique ids for each note using nanoid, saved notes display correctly

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,14 @@
-//const path = require('path');
 const router = require('express').Router();
 const notes  = require('../../db/db.json');
 const createNewNote = require('../../lib/notes');
+const { nanoid } = require('nanoid');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+    req.body.id = nanoid(3);
 
     const note = createNewNote(req.body, notes)
     
